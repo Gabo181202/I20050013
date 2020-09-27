fdc69c1f8ba9b593a67a34ad0f885ce77d13c5d0
Gabo181202 <71952683+Gabo181202@users.noreply.github.com>
2020-09-26 23:14:05 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear o declarar las variables a utilizar
+            int n1, n2, n3, s;
+
+            //Preguntar el valor de un numero
+            Console.Write("N1: ");
+            n1 = Int32.Parse(Console.ReadLine());
+            //Preguntar el valor del segundo numero
+            Console.Write("N2: ");
+            n2 = Int32.Parse(Console.ReadLine());
+            //Preguntar el valor del tercer numero
+            Console.Write("N3: ");
+            n3 = Int32.Parse(Console.ReadLine());
+            //Sumarlos
+            s = n1 + n2 + n3;
+            //Mostrar el resultado
+
+            Console.Write("Resultado "+s);
+
+            Console.ReadKey();
+        }
+    }
+}
